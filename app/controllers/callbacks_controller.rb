2d61ed37d654ffa023637fa6bfe25cfcaa8199be
h4hardikonly <h4hardikonly@gmail.com>
2016-05-23 12:38:43 +0530

    modified callback logic to allow access to only those who are part of coupa organization

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,11 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    login = request.env["omniauth.auth"]['extra']['raw_info']['login'] rescue nil
+    if GIT_INTERACTOR.organization_member?('coupa', login)
+      @user = User.from_omniauth request.env["omniauth.auth"]
+      sign_in_and_redirect @user
+    else
+      # don't have access scsenario
+    end
   end
 end
