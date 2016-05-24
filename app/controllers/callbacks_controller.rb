4bdd750fd74a76d54f0aa2034dae71bb4fb4d486
h4hardikonly <h4hardikonly@gmail.com>
2016-05-24 16:41:42 +0530

    added loggers in callbacks controller

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,10 +1,13 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     login = request.env["omniauth.auth"]['extra']['raw_info']['login'] rescue nil
+    logger.debug "user with login #{login} is trying to sign up via Git"
     if GIT_INTERACTOR.organization_member?('coupa', login)
+      logger.debug "user is member of coupa org, granting access"
       @user = User.from_omniauth request.env["omniauth.auth"]
       sign_in_and_redirect @user
     else
+      logger.debug "user is not member of coupa org, f**k off"
       # don't have access scsenario
     end
   end
