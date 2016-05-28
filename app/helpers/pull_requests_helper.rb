e1c45176d35355176e4f58cf459d1e42e9ca7cc0
h4hardikonly <h4hardikonly@gmail.com>
2016-05-28 23:03:47 +0530

    minor refactoring

--- app/helpers/pull_requests_helper.rb
+++ app/helpers/pull_requests_helper.rb
@@ -1,2 +1,10 @@
 module PullRequestsHelper
+  def required_action_text(pr)
+    case pr.state
+    when 'conflict'
+      "Pull Request in conflict with #{pr.against}, please rebase and solve conflicts"
+    when 'unstable'
+      'One of the check is failing for pull request'
+    end
+  end
 end
