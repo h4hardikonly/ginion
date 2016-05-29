7a2000bead608c611a23014ff74ee01bc395343d
h4hardikonly <h4hardikonly@gmail.com>
2016-05-29 16:57:04 +0530

    restructured homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user! 
 
   def homepage
-    @queued_prs = PullRequest.where(queued_by: current_user, state: 'open')
-    @prs = PullRequest.where(queued_by: current_user, state: ['conflict', 'unstable'])
+    @queued_prs  = PullRequest.where(queued_by: current_user, state: 'open')
+    @stucked_prs = PullRequest.where(queued_by: current_user, state: ['conflict', 'unstable'])
+    @merge_closed_prs   = PullRequest.where(queued_by: current_user, state: ['merge_closed'])
+    @unmerge_closed_prs = PullRequest.where(queued_by: current_user, state: ['close_without_merge'])
   end
 end
